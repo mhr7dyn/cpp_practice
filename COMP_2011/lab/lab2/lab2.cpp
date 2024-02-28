89489d8bdac5f42b0b020c4e4e4235d26c0d88e2
mhr7dyn <139538957+mhr7dyn@users.noreply.github.com>
2024-02-28 03:10:58 +0000

    added lab2

---
+++ COMP_2011/lab/lab2/lab2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to your second C++ lab!" << endl;
+    cout << "In this program, let us determine whether an input integer is a perfect square." << endl;
+    cout << "If the input is negative, it is invalid." << endl;
+    cout << "If the input is non-negative, it may or may not be a perfect square." << endl;
+    cout << "If the input is a perfect square, let's find its non-negative integer square root and exit the program." << endl;
+    cout << endl;
+
+    /////////////////////////////////////////////////
+    // TODO: Complete the code below
+    // Your Code Here
+
+
+
+    /////////////////////////////////////////////////
+    
+    return 0;
+}
